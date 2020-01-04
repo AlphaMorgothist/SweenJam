c60fbcaa5aeeb3be18c311d40ed35cfc1dc7c6e9
Sweeny <44808412+MikeSweeny@users.noreply.github.com>
2020-01-04 11:59:07 -0500

    Created prefabs for the FoW system for level design

--- BTS/Assets/Scripts/Maps/Components/Traps/Ladders/LadderTrap.cs
+++ BTS/Assets/Scripts/Maps/Components/Traps/Ladders/LadderTrap.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class LadderTrap : TrapComponent
 {
+    public bool update;
+    public GameObject basicLadderSprite;
+    public GameObject unstableLadderSprite;
+    public GameObject brokenLadderSprite;
+    public GameObject fireLadderSprite;
+    public GameObject iceLadderSprite;
+    public GameObject slowLadderSprite;
+
+    private void Update()
+    {
+        if (update)
+        {
+            UpdateTiles();
+        }
+    }
+
+    private void Start()
+    {
+        basicLadderSprite = gameObject.transform.GetChild(0).gameObject;
+        unstableLadderSprite = gameObject.transform.GetChild(1).gameObject;
+        brokenLadderSprite = gameObject.transform.GetChild(2).gameObject;
+        fireLadderSprite = gameObject.transform.GetChild(3).gameObject;
+        iceLadderSprite = gameObject.transform.GetChild(4).gameObject;
+        slowLadderSprite = gameObject.transform.GetChild(5).gameObject;
+    }
     protected override void ApplyEffect(AICharacter character)
     {
         if (trapType == TrapTypes.FIRE)
@@ -31,19 +56,47 @@ public class LadderTrap : TrapComponent
         {
             // Ladders can be Basic, Unstable(breakable), Broken, On Fire, Icy, or Slow(Sticky) //
             case TrapTypes.BASIC:
+                TileDeActivator();
+                basicLadderSprite.SetActive(true);
+                update = false;
                 break;
             case TrapTypes.UNSTABLE:
+                TileDeActivator();
+                unstableLadderSprite.SetActive(true);
+                update = false;
                 break;
             case TrapTypes.BROKEN:
+                TileDeActivator();
+                brokenLadderSprite.SetActive(true);
+                update = false;
                 break;
             case TrapTypes.FIRE:
+                TileDeActivator();
+                fireLadderSprite.SetActive(true);
+                update = false;
                 break;
             case TrapTypes.ICE:
+                TileDeActivator();
+                iceLadderSprite.SetActive(true);
+                update = false;
                 break;
             case TrapTypes.SLOW:
+                TileDeActivator();
+                slowLadderSprite.SetActive(true);
+                update = false;
                 break;
             default:
                 break;
         }
+    }
+
+    private void TileDeActivator()
+    {
+        basicLadderSprite.SetActive(false);
+        unstableLadderSprite.SetActive(false);
+        brokenLadderSprite.SetActive(false);
+        fireLadderSprite.SetActive(false);
+        iceLadderSprite.SetActive(false);
+        slowLadderSprite.SetActive(false);
     }
 }
