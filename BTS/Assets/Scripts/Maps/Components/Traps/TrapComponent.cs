3c574359281692081911ab8a112cdebb521a233d
Sweeny <44808412+MikeSweeny@users.noreply.github.com>
2020-01-04 03:25:37 -0500

    Breakable floors and walls now created in prefabs

--- BTS/Assets/Scripts/Maps/Components/Traps/TrapComponent.cs
+++ BTS/Assets/Scripts/Maps/Components/Traps/TrapComponent.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class TrapComponent : MapComponent
 {
+    protected bool isActive;
 
+    private void Update()
+    {
+        if(isActive)
+        {
+            OnActivate();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("AI"))
+        {
+            ApplyEffect();
+        }
+    }
+
+    virtual protected void ApplyEffect()
+    {
+
+    }
+
+    virtual protected void OnActivate()
+    {
+
+    }
 }
