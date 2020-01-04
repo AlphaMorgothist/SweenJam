1bdcdf6b34bc92000dcae9340467f7e97b2c16b7
Sweeny <44808412+MikeSweeny@users.noreply.github.com>
2020-01-04 15:38:23 -0500

    Platform trap prefab created prototype ready to go!

--- BTS/Assets/Scripts/Maps/Components/Traps/Platforms/PlatformTrap.cs
+++ BTS/Assets/Scripts/Maps/Components/Traps/Platforms/PlatformTrap.cs
@@ -4,6 +4,32 @@ using UnityEngine;
 
 public class PlatformTrap : TrapComponent
 {
+    // Platform traps include movable, breakable, and status affectable platforms
+    public GameObject basicPlatformSprite;
+    public GameObject unstablePlatformSprite;
+    public GameObject firePlatformSprite;
+    public GameObject icePlatformSprite;
+    public GameObject slowPlatformSprite;
+
+
+    private void Start()
+    {
+        basicPlatformSprite = gameObject.transform.GetChild(0).GetChild(0).gameObject;
+        unstablePlatformSprite = gameObject.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(0).gameObject;
+        firePlatformSprite = gameObject.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(1).gameObject;
+        icePlatformSprite = gameObject.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(2).gameObject;
+        slowPlatformSprite = gameObject.transform.GetChild(0).GetChild(0).GetChild(0).GetChild(3).gameObject;
+    }
+    protected override void OnActivate()
+    {
+        base.OnActivate();
+        if (trapType == TrapTypes.UNSTABLE)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+
     protected override void ApplyEffect(AICharacter character)
     {
         if (trapType == TrapTypes.FIRE)
@@ -18,5 +44,49 @@ public class PlatformTrap : TrapComponent
         {
             //character gets slowed for a short duration
         }
+    }
+
+    protected override void UpdateTiles()
+    {
+        base.UpdateTiles();
+        switch (trapType)
+        {
+            // Platforms can be Basic, Unstable(breakable), Broken, On Fire, Icy, or Slow(Sticky) //
+            case TrapTypes.BASIC:
+                TileDeActivator();
+                basicPlatformSprite.SetActive(true);
+                update = false;
+                break;
+            case TrapTypes.UNSTABLE:
+                TileDeActivator();
+                unstablePlatformSprite.SetActive(true);
+                update = false;
+                break;
+            case TrapTypes.FIRE:
+                TileDeActivator();
+                firePlatformSprite.SetActive(true);
+                update = false;
+                break;
+            case TrapTypes.ICE:
+                TileDeActivator();
+                icePlatformSprite.SetActive(true);
+                update = false;
+                break;
+            case TrapTypes.SLOW:
+                TileDeActivator();
+                slowPlatformSprite.SetActive(true);
+                update = false;
+                break;
+            default:
+                break;
+        }
+    }
+    protected override void TileDeActivator()
+    {
+        base.TileDeActivator();
+        unstablePlatformSprite.SetActive(false);
+        firePlatformSprite.SetActive(false);
+        icePlatformSprite.SetActive(false);
+        slowPlatformSprite.SetActive(false);
     }
 }
